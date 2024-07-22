8577a51d65e95286cc1467721093451f0e29ee71
juhid30 <55.juhideore@gmail.com>
2024-07-22 13:59:25 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,51 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainLanding from "./components/MainLanding.jsx";
+import OAuthCallback from "./components/OAuthCallback.jsx";
+import StudentDashboard from "./components/StudentDashboard.jsx";
+import SecurityDashboard from "./components/SecurityDashboard.jsx";
+import CamSurveillance from "./components/CamSurveillance.jsx";
+import SecurityCheckIn from "./components/SecurityCheckIn.jsx";
 import { useState } from "react";
 
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import "./App.css";
-
-// import Sidebar from './components/Sidebar.jsx'
-import Layout from "./components/Layout.jsx";
-import MedicalHistory from "./components/MedicalHistory.jsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ActivitySummary from "./components/ActivitySummary.jsx";
-import AvatarCanvas from "./components/Avatar/AvatarCanvas.jsx";
-import Alert from "./components/Alert.jsx";
-import PatientInfo from "./components/PatientInfo.jsx";
-import LeaveApplication from "./components/LeaveApplication.jsx";
-import LeaveReview from "./components/LeaveReview.jsx";
-
 import WardenDashboard from "./components/WardenDashboard.jsx";
-import StudentDashboard from "./components/StudentDashboard.jsx";
-import HelpAsk from "./components/HelpAsk.jsx";
-
-import DownloadEntries from "./components/DownloadEntries.jsx";
-import MedicineTracker from "./components/MedicineTracker.jsx";
-import SecurityCheckIn from "./components/SecurityCheckIn.jsx";
+import PatientInfo from "./components/PatientInfo.jsx";
+import CommunityChat from "./components/Community.jsx";
+import StudentLocation from "./components/StudentLocation.jsx";
+// import other components as needed
 
 function App() {
+  console.log("APP RENDERED");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
-    <>
-      <Router>
-        <Layout>
-          {/* CHILD COMPONENTS HERE */}
-          {/* TEST */}
-          {/* <MedicineTracker /> */}
-          {/* <DownloadEntries /> */}
-          {/* <LeaveApplication /> */}
-          {/* <LeaveReview /> */}
-          {/* <Alert /> */}
-          {/* <AvatarCanvas /> */}
-          {/* <MedicalHistory /> */}
-          {/* <CamSurveillance /> */}
-          {/* <PatientInfo /> */}
-          {/* <SecurityCheckIn /> */}
-          <StudentDashboard/>
-          {/* <StudentDashboard/> */}
-          {/* <HelpAsk /> */}
-        </Layout>
-      </Router>
-    </>
+    <Router>
+      {/* Main Landing is rendered as a route */}
+      {/* <WardenDashboard/> */}
+
+      {/* <CommunityChat /> */}
+      <Routes>
+        <Route path="/oauth/callback" element={<OAuthCallback />} />
+        <Route path="/" element={<MainLanding />} />
+      </Routes>
+    </Router>
   );
 }
 
